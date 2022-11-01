339e0f3c81d6102b686f633ebbf953b829a6d7e3
Нуржан <Studerzhan@gmail.com>
2022-11-01 16:01:54 +0600

    dd

--- historyCms/settings/dev.py
+++ historyCms/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0(%wg9!8s&+t3ew+)^n-w+*7fyyg=#+u#n9k-!_cs3ofvhf5l4"
